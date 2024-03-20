b797e1b3763c885b890b2fcb0953bcfac7999a82
Pepephant <2983474545@qq.com>
2024-03-20 13:30:10 +0800

    h5 record test succeeded

---
+++ pages/test/test.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<div class="container">
+			<button v-for="(item, index) in pathList" :key="index" @click="TestPage(item.path)"> {{ item.lebal }} </button>
+		</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				pathList:[
+					{
+						path: "/pages/recTest/page_index",
+						lebal: "测试音频"
+					},
+					{
+						path: "/pages/test-camera/test-camera",
+						lebal: "测试摄像头"
+					}
+				]
+			}
+		},
+		methods: {
+			TestPage (url) {
+				uni.navigateTo ({
+					url
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ unpackage/dist/build/.nvue/app.js
@@ -0,0 +1,5 @@
+Promise.resolve("./pages/recTest/page_nvue.js").then((res) => {
+  res.length;
+});
+Promise.resolve("./app.css.js").then(() => {
+});
