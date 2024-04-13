6b655e6e7a384d8b5d9dfa72cd685f65e37a65ff
Pepephant <2983474545@qq.com>
2024-04-13 21:00:46 +0800

    changed global data for a transfer of servers

--- App.vue
+++ App.vue
@@ -2,7 +2,7 @@
 	export default {
 		globalData: {  
 			backUrl: 'http://82.157.124.83:51603',
-			baseUrl: 'http://127.0.0.1:8000'
+			baseUrl: 'http://192.168.177.100:8000'
 		},
 		onLaunch: function() {
 			console.log('App Launch')
